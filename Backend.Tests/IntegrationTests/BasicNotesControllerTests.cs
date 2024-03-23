8c616c81a84f45d8bb3eef577bcb7861ce5ff42c
Kyle Rego <regoky@outlook.com>
2024-03-23 12:31:38 -0400

    Initial work Backend.Tests project

---
+++ Backend.Tests/IntegrationTests/BasicNotesControllerTests.cs
@@ -0,0 +1,42 @@
+using System.Net;
+using System.Net.Http.Json;
+using AnkiBooks.Backend.DbContext;
+using AnkiBooks.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace AnkiBooks.Backend.Tests.IntegrationTests;
+
+public class BasicNotesControllerTests : IClassFixture<TestServerFactory<Program>>
+{
+    private readonly TestServerFactory<Program> _factory;
+
+    public BasicNotesControllerTests(TestServerFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task Post_BasicNotes_CreatesBasicNote()
+    {
+        using IServiceScope scope = _factory.Services.CreateScope();
+        ApplicationDbContext dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        Article article = new("Test article");
+        dbContext.Articles.Add(article);
+        await dbContext.SaveChangesAsync();
+
+        BasicNote basicNote = new()
+        {
+            Front = "Front",
+            Back = "Back",
+            OrdinalPosition = 0,
+            ArticleId = article.Id
+        };
+
+        HttpClient client = _factory.CreateClient();
+
+        HttpResponseMessage response = await client.PostAsJsonAsync("api/BasicNotes", basicNote);
+
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+    }
+}
---
+++ Backend.Tests/TestServerFactory.cs
@@ -0,0 +1,51 @@
+using System.Data.Common;
+using AnkiBooks.Backend.DbContext;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnkiBooks.Backend.Tests;
+
+public class TestServerFactory<TProgram>
+    : WebApplicationFactory<TProgram> where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            ServiceDescriptor? dbContextDescriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                    typeof(DbContextOptions<ApplicationDbContext>));
+
+            if (dbContextDescriptor != null)
+            {
+                services.Remove(dbContextDescriptor);
+            }
+
+            ServiceDescriptor? dbConnectionDescriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                    typeof(DbConnection));
+
+            if (dbConnectionDescriptor != null)
+            {
+                services.Remove(dbConnectionDescriptor);
+            }
+
+            services.AddSingleton<DbConnection>(container =>
+            {
+                var connection = new SqliteConnection("DataSource=:memory:");
+                connection.Open();
+
+                return connection;
+            });
+
+            services.AddDbContext<ApplicationDbContext>((container, options) =>
+            {
+                var connection = container.GetRequiredService<DbConnection>();
+                options.UseSqlite(connection);
+            });
+        });
+
+        builder.UseEnvironment("Development");
+    }
+}
