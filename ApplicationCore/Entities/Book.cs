ea6d75f746bda7a76d659a8b34c1f269cb6fec06
Kyle Rego <regoky@outlook.com>
2024-05-03 19:42:15 -0400

    Add a second migration to ensure applied

--- ApplicationCore/Entities/Book.cs
+++ ApplicationCore/Entities/Book.cs
@@ -1,3 +1,8 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+using AnkiBooks.ApplicationCore.Identity;
+
 namespace AnkiBooks.ApplicationCore.Entities;
 
 public class Book : EntityBase, IPublicViewable
@@ -5,6 +10,12 @@ public class Book : EntityBase, IPublicViewable
     public bool PublicViewable { get; set; } = false;
 
     public string Title { get; set; } = "New book";
+
+    [ForeignKey(nameof(User))]
+    public string? UserId { get; set; }
+
+    [JsonIgnore]
+    public ApplicationUser? User { get; set; }
 }
 
 internal interface IPublicViewable
---
+++ Infrastructure/Migrations/20240503233607_AddUserIdToBooks.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIdToBooks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_AspNetUsers_ApplicationUserId",
+                table: "Books");
+
+            migrationBuilder.RenameColumn(
+                name: "ApplicationUserId",
+                table: "Books",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Books_ApplicationUserId",
+                table: "Books",
+                newName: "IX_Books_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_AspNetUsers_UserId",
+                table: "Books",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_AspNetUsers_UserId",
+                table: "Books");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Books",
+                newName: "ApplicationUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Books_UserId",
+                table: "Books",
+                newName: "IX_Books_ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_AspNetUsers_ApplicationUserId",
+                table: "Books",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
